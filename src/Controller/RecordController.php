11240039bad7675638e8929ec776365f35d5b2af
Seifo <seifallah27@gmail.com>
2020-08-19 11:11:26 +0200

    Ajout La liste des artistes

---
+++ src/Controller/RecordController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArtistRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RecordController extends AbstractController
+{
+    /**
+     * liste des artistes
+     * @Route("/artist", name="artist_list")
+     */
+    public function index(ArtistRepository $repository)
+    {
+        return $this->render('record/artist_list.html.twig', [
+            'artist_list' => $repository->findAll(),
+        ]);
+    }
+}
