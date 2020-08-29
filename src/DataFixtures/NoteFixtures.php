1d30518b42a84bf4c85a49564383706c09c38b3b
Seifo <seifallah27@gmail.com>
2020-08-29 13:51:56 +0200

    Fixture Note

--- src/DataFixtures/NoteFixtures.php
+++ src/DataFixtures/NoteFixtures.php
@@ -2,13 +2,30 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
+use App\Entity\Note;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class NoteFixtures extends BaseFixture
+class NoteFixtures extends BaseFixture implements DependentFixtureInterface
 {
     public function loadData()
     {
-        
+        $this->createMany(1000, 'note', function(){
+            return (new Note())
+                        ->setValue($this->faker->randomDigit(5))
+                        ->setComment($this->faker->unique()->catchPhrase)
+                        ->setCreatedAt($this->faker->dateTimeBetween('-2 years'))
+                        ->setRecord($this->getRandomReference('record'))
+                        ->setUser($this->getRandomReference('user'))
+                        ;
+        });
     }
+
+    public function getDependencies()
+    {
+        return [
+            RecordFixtures::class,
+            UserFixtures::class
+        ];
+    }
+
 }
